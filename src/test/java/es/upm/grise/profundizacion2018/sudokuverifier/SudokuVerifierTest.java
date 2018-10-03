105a291f43f87c6eb5a5b9c94cb2301a71ba36bf
PabloFdez <pablofernandez.fi@gmail.com>
2018-10-03 11:35:18 +0200

    test edit

--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -12,27 +12,27 @@ public class SudokuVerifierTest {
     
 	@Test
 	public void sudoku_OK() {
- 		assertEquals(0, sudokuVer.verify("sudokuOK"));
+ 		assertEquals(0, sudokuVer.verify("984576213513482967726139548631947852495268371872351694257613489368794125149825736"));
     		}
 	
 	@Test
 	public void sudoku_Negativo_O_Cero() {
- 		assertEquals(-1, sudokuVer.verify("sudokuE1"));
+ 		assertEquals(-1, sudokuVer.verify("980576213513482967726139548631947852495268371872351694257613489368794125149825736"));
     		}
 	@Test
 	public void sudoku_Repetido_Subgrid() {
- 		assertEquals(-2, sudokuVer.verify("sudokuE2"));
+ 		assertEquals(-2, sudokuVer.verify("984976213413482967726139948631947852495268371872351694257613489368794125149825736"));
     		}
 	@Test
 	public void sudoku_Repetido_Fila() {
- 		assertEquals(-3, sudokuVer.verify("sudokuE3"));
+ 		assertEquals(-3, sudokuVer.verify("984976213513482567726135948631947852495268371872351694257613489368794125149825736"));
     		}
 	@Test
 	public void sudokuRepetido_Col() {
- 		assertEquals(-4, sudokuVer.verify("sudokuE4"));
+ 		assertEquals(-4, sudokuVer.verify("984576213513482967726139548631947852495268371872351694257613489368794125941825736"));
     		}
 	@Test
 	public void sudoku_Error_Length() {
- 		assertEquals(-5, sudokuVer.verify("sudokuE5"));
+ 		assertEquals(-5, sudokuVer.verify("99984576213513482967726139548631947852495268371872351694257613489368794125149825736"));
     		}
 }
